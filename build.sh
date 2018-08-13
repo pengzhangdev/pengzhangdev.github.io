1887abe6c29b7641422ab2b17739cef271e9cb73
wertherzhang <pengzhangdev@gmail.com>
2018-08-13 21:49:46 +0800

    update documents

--- build.sh
+++ build.sh
@@ -5,6 +5,7 @@ git add ./
 git commit -m "update documents"
 
 sudo docker run --rm -it -p 8000:8000 -v ${PWD}:/docs squidfunk/mkdocs-material build
+sudo chown werther:werther -R site
 
 if [ -d /tmp/_site ]; then
     rm -rf /tmp/_site
